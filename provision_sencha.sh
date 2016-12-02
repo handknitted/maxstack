61159be114aa407288fc536ad710c11af3c26e2e
David C Kennedy <david.c.kennedy@hpe.com>
2016-12-02 11:59:15 +0000

    Put datadog api key into the env for setup

--- provision_sencha.sh
+++ provision_sencha.sh
@@ -11,8 +11,7 @@ else
     git clone https://git.openstack.org/openstack-dev/devstack
 fi
 
-export DD_API_KEY=dfec0ce905031315208b2236dbd911ff 
 curl https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh -o install_agent.sh
 sudo chmod +x install_agent.sh
-sudo ./install_agent.sh
+sudo DD_API_KEY=${DD_API_KEY} ./install_agent.sh
 
